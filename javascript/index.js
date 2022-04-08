118041516574ef87702579a29a1cb3356593e453
AtwijukireBrendah <kire5brenda@gmail.com>
2022-04-09 01:33:56 +0300

    made changes to home page

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,35 @@
-(function($){
-   console.log("it works well")
-})(document);
+const products = [
+   {
+      id: "P001",
+      name: "Cola",
+      price: 2000,
+      image: "http://myimage.png"
+   },
+   {
+      id: "P002",
+      name: "Ice tea",
+      price: 2500,
+      image: "http://myimage.png"
+   },
+   {
+      id: "P003",
+      name: "Fata",
+      price: 1000,
+      image: "http://myimage.png"
+   }
+]
+
+function searchProduct(name){
+   return products.filter(function(product) {
+      return product.name.toLowerCase() == name.toLowerCase()
+   })
+}
+function registerEvents(){
+   O("btnSearch").addEventListener("click", function(e) {
+      let searchInput = O("searchbar").value
+      let searchResults = searchProduct(searchInput)
+      console.log(searchResults)
+   })
+}
+
+registerEvents()
---
+++ javascript/signup.js
@@ -0,0 +1,17 @@
+(function($){
+    "use strict"
+
+    function signUpAction(userDetails){
+        alert("You just signup")
+    }
+
+    function logInAction(userDetails){
+        alert("You just logged in")
+    }
+
+    function forgotPassword(){
+
+    }
+
+
+})(document);
